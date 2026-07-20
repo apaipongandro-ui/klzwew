8563cfc1ab6fbbb992acc7f5283b1d08d015a347
apaipongandro-ui <apaipongandro@gmail.com>
2026-07-20 23:00:14 +0700

    Create tailwind.config.js

---
+++ CONFIGURATION FILES/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'canva-dark': '#0e131f',
+        'canva-card': '#181e2a',
+        'canva-cyan': '#00C4CC',
+        'canva-purple': '#7D2AE8',
+      }
+    },
+  },
+  plugins: [],
+}
