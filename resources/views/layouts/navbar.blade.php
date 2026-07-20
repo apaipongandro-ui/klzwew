9fc41e80e34cab6f81073c5ad57aea35a8878713
apaipongandro-ui <apaipongandro@gmail.com>
2026-07-20 22:52:31 +0700

    Create navbar.blade.php

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/layouts/navbar.blade.php -->
+<nav class="navbar navbar-expand-lg" style="background: var(--canva-surface); border-bottom: 1px solid #2a3447;">
+    <div class="container-fluid">
+        <a class="navbar-brand canva-gradient-text" href="/">
+            <i class="fas fa-server"></i> Pterodactyl Canva
+        </a>
+        <div class="d-flex ms-auto">
+            <span class="text-white"><i class="fas fa-user-circle"></i> Admin Xtreme</span>
+        </div>
+    </div>
+</nav>
